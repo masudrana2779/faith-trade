d15d9ac695aa19b08c0fbe438cbd42f7daf2ff58
Masud Rana <masudrana2779@gmail.com>
2021-06-01 14:23:20 +0600

    banner slider animation done

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,9 +30,11 @@
         function bannerSlider() {
             $(".bannerSlider").owlCarousel({
                 items: 1,
-                nav: false,
+                nav: true,
                 dots: false,
                 loop: true,
+                lazyLoad: true,
+                // autoplaySpeed: 1000,
                 autoplay: true,
                 responsive: {
                     0: {
@@ -51,62 +53,6 @@
         }
 
         bannerSlider();
-
-
-
-        function latestSlider() {
-            $(".latestSlider").owlCarousel({
-                items: 3,
-                // margin: 30,
-                nav: true,
-                responsive: {
-                    0: {
-                        items: 1,
-                        nav: false,
-                    },
-                    480: {
-                        items: 2,
-                        nav: false,
-                    },
-                    768: {
-                        items: 2
-                    },
-                    1920: {
-                        items: 3
-                    }
-                },
-            });
-        }
-
-        latestSlider();
-
-        function footerLogoSlider() {
-            $(".footerLogoSlider").owlCarousel({
-                items: 5,
-                margin: 30,
-                // nav: true,
-                freeDrag: true,
-                loop: true,
-                autoplay: true,
-                slideBy: 1,
-                responsive: {
-                    0: {
-                        items: 2,
-                    },
-                    480: {
-                        items: 2,
-                    },
-                    768: {
-                        items: 3
-                    },
-                    1920 :{
-                        items: 5
-                    }
-                }
-            });
-        }
-
-        footerLogoSlider();
 
     });
 
